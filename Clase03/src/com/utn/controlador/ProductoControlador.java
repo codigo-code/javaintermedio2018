5f3ca68d422a7ec1f9b9fb95d4fbc153a3d39a67
DiegoB <diegohbarletta@gmail.com>
2018-08-30 22:12:54 -0300

    final de clase 3 codigo parcial

--- Clase03/src/com/utn/controlador/ProductoControlador.java
+++ Clase03/src/com/utn/controlador/ProductoControlador.java
@@ -1,5 +1,27 @@
 package com.utn.controlador;
 
+import com.utn.modelo.Camisa;
+import com.utn.modelo.Tipo;
+import com.utn.modelo.Producto;
+
 public class ProductoControlador {
+	
+	public Producto creoProducto(String tipo,String marca, Object talle, double precio) {
+		
+		Producto p = null; //creamos variable tipo producto la inicializamos a null y 
+		//luego en cada parte del switch le damos la instancia correspondiente
+		
+		switch (tipo) {
+		
+		case CAMISA:
+			p = new Camisa();
+			asignoValores(p, marca, talle,precio);
+		}
+		
+		return p;
+	}
+	
+	//private producto
+	
 
 }
