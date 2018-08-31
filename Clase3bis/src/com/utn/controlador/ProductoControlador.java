45acf8c0ee8eb68092eef7cbd33f794f26da20bd
Dante Panella <dante.panella@neoris.cxnetworks.net>
2018-08-30 22:12:22 -0300

    clase 3 arraylist java utn turno noche java 2018 java intermedio java

--- Clase3bis/src/com/utn/controlador/ProductoControlador.java
+++ Clase3bis/src/com/utn/controlador/ProductoControlador.java
@@ -1,6 +1,7 @@
 package com.utn.controlador;
 
 import java.awt.PageAttributes;
+import java.util.ArrayList;
 
 import com.utn.modelo.Camisa;
 import com.utn.modelo.Pantalon;
@@ -9,6 +10,28 @@ import com.utn.modelo.Tipo;
 import com.utn.modelo.Zapatilla;
 
 public class ProductoControlador {
+
+	/*
+	 * Puede tener dos tipos 1) tipado 2) no tipado
+	 * 
+	 * Ejemplo 1) ArrayList<Producto> listaProdcuto listaProducto.add(Camisa,
+	 * Zapatilla, Pantalon)
+	 * 
+	 * Ejemplo 2) ArrayList listaObjecto listaObjeto.add(1);
+	 * listaObjecto.add("hola mundo"); listaObjecto.add(Camisa);
+	 * 
+	 * 
+	 */
+
+	private ArrayList<Producto> listaProducto; // no lo inicializo
+	// private String nombre=""; //declaracion de la variable
+
+	public ProductoControlador() {
+		// inicializo la lista
+		// this.nombre="lucas"; //inicializacion de la variable darle valor o crear
+		// instancia
+		this.listaProducto = new ArrayList<Producto>();
+	}
 
 	public Producto creoProducto(Tipo tipo, String marca, Object talle, double precio) {
 
@@ -29,6 +52,29 @@ public class ProductoControlador {
 			System.err.println("No conozco al tipo");
 			return null;
 		}
+	}
+
+	// asigno producto a lista
+	public void asignoProductoLista(Producto product1) {
+		this.listaProducto.add(product1);
+	}
+
+	public void muestroProductoLista() {
+		System.out.println("Mostramos foreach por java 7 ");
+		// java 7
+		for (Producto item : listaProducto) {
+			System.out.println(item.getClass().getSimpleName() + " => " + item.getMarca() + " " + item.getPrecio() + " "
+					+ item.getTalle());
+		}
+		System.out.println("=======================");
+		System.out.println("Mostramos foreach por java 8 ");
+
+		// java 8 aplicando lambda
+		listaProducto.forEach(item -> {
+			System.out.println(item.getClass().getSimpleName() + " => " + item.getMarca() + " " + item.getPrecio() + " "
+					+ item.getTalle());
+		});
+		System.out.println("=======================");
 	}
 
 }
--- Clase3bis/src/com/utn/vista/Test.java
+++ Clase3bis/src/com/utn/vista/Test.java
@@ -15,34 +15,16 @@ public class Test {
 		 
 		//bajo la instancia de una camisa
 		 Producto p1= pc.creoProducto(Tipo.CAMISA, "Nike", "M", 2000);
-		
 		 Producto p2 = pc.creoProducto(Tipo.ZAPATILLA, "Avidas", 12, 3500);
-		 
 		 Producto p3 = pc.creoProducto(Tipo.PANTALON, "Legacy", 58.5, 4000);
 		 
-		 Producto[] listaProducto = new Producto[3];
-		 listaProducto[0]=p1;
-		 listaProducto[1]=p2;
-		 listaProducto[2]=p3;
 		 
-		 
-		 
-		 //Foreach de java 7
-//		 for (Producto item : listaProducto) {
-//			 System.out.println(item.getClass().getSimpleName()+" => "+  item.getMarca()+ " " + item.getPrecio() + " " + item.getTalle());
-//		 }
-
-		 
-		 //java 8 foreach 
-		ArrayList<Producto> ProductList = new ArrayList<>();
-		ProductList.add(p1);
-		ProductList.add(p2);
-		ProductList.add(p3);
-		ProductList.forEach(item ->{
-			 System.out.println(item.getClass().getSimpleName()+" => "+  item.getMarca()+ " " + item.getPrecio() + " " + item.getTalle());
-		});
+		pc.asignoProductoLista(p1);
+		pc.asignoProductoLista(p2);		 
+		pc.asignoProductoLista(p3);
 		
-		 
+		pc.muestroProductoLista();
+		
 		 // forma cabeza	
 //		 System.out.println(p1.getClass().getSimpleName()+" => "+  p1.getMarca()+ " " + p1.getPrecio() + " " + p1.getTalle());
 //		 System.out.println(p2.getClass().getSimpleName()+" => "+ p2.getMarca()+ " " + p2.getPrecio() + " " + p2.getTalle());
