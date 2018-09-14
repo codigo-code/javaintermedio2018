63f71b60da2da77181273853905d75702457077b
Dante Panella <dante.panella@neoris.cxnetworks.net>
2018-09-13 22:03:18 -0300

    update clase 7 repaso antes del examen ESTUDIEN X FAVOR

--- Clase7/src/com/utn/controlador/PeliculaControlador.java
+++ Clase7/src/com/utn/controlador/PeliculaControlador.java
@@ -16,25 +16,46 @@ public class PeliculaControlador {
 		this.listaPeliculas = new ArrayList<Pelicula>();
 	}
 
-	public void creoPelicula(Genero genero) {
-		
+	
+	
+	public void creoPeliculaxGenero(Genero genero) {
+
 		switch (genero) {
 		case ACCION:
-				this.listaPeliculas.add(new DVD("Duro de matar", genero, 40.0)); 
-				this.listaPeliculas.add(new VHS("Duro de matar", genero, 40.0)); 
-				this.listaPeliculas.add(new BlueRay("Duro de m	atar", genero, 40.0)); 
+			this.listaPeliculas.add(new DVD("Duro de matar", genero, 40.0));
+			this.listaPeliculas.add(new VHS("Duro de matar", genero, 10.0));
+			this.listaPeliculas.add(new BlueRay("Duro de m	atar", genero, 140.0));
 			break;
 		case TERROR:
-
+			this.listaPeliculas.add(new DVD("the ring", genero, 40.0));
+			this.listaPeliculas.add(new VHS("the ring", genero, 12.0));
+			this.listaPeliculas.add(new BlueRay("the ring", genero, 100.0));
 			break;
 		case PORNO:
-
+			this.listaPeliculas.add(new DVD("50 sombras de grey", genero, 40.0));
+			this.listaPeliculas.add(new VHS("50 sombras de grey", genero, 12.0));
+			this.listaPeliculas.add(new BlueRay("50 sombras de grey", genero, 100.0));
 			break;
-
 		default:
-		
+			System.out.println("Desconozco el genero");
 			break;
 		}
+	}
+
+	public void muestroReportePelicula() {
+		// estandar
+		for (Pelicula item : listaPeliculas) {
+			System.out.println("Genero " + item.getGenero() + " nombre " + item.getNombre());
+		}
+System.out.println("==================================");
+		// lambda solo para java 8
+		this.listaPeliculas.forEach(item -> {
+			System.out.println("Genero " + item.getGenero() + " nombre " + item.getNombre());
+		});
+	}
+
+	public ArrayList<Pelicula> devuelvoListaPeliculas() {
+		return this.listaPeliculas;
 	}
 
 }
---
+++ Clase7/src/com/utn/vista/Test.java
@@ -0,0 +1,16 @@
+package com.utn.vista;
+
+import com.utn.controlador.ClienteControlador;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+			ClienteControlador cc = new ClienteControlador();
+			
+			cc.alquiloPelicula();
+			
+			
+	}
+
+}
